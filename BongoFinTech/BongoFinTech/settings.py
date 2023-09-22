72678cdb62890a55cb3357f7d14d558b7c9f8e6b
Md. Nihad Hossain <hnihadgo75@gmail.com>
2023-09-22 08:07:38 +0600

    Update settings.py
    done

--- BongoFinTech/BongoFinTech/settings.py
+++ BongoFinTech/BongoFinTech/settings.py
@@ -58,7 +58,7 @@ ROOT_URLCONF = 'BongoFinTech.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [BASE_DIR/'templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -120,6 +120,15 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = 'static/'
+# print(BASE_DIR)
+STATICFILES_DIR =[
+    BASE_DIR /'core/static'
+]
+# print(STATICFILES_DIR)
+STATIC_ROOT= BASE_DIR /'core/static'
+
+MEDIA_ROOT=BASE_DIR / 'core/media'
+MEDIA_URL='media/
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
